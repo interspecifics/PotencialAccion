f226a454a1dcd59ef4e653e85bbc37b35dbdb494
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-12-10 00:19:29 -0600

    initial commit of a python osc server for the headset

---
+++ python/emoOscServer/emoOscServer.py
@@ -0,0 +1,31 @@
+from emokit.emotiv import Emotiv
+from OSC import OSCClient, OSCMessage
+import gevent
+
+OSC_OUT_HOST = "localhost"
+OSC_OUT_PORT = 8444
+
+if __name__ == "__main__":
+    mOscClient = OSCClient()
+    mOscClient.connect( (OSC_OUT_HOST,OSC_OUT_PORT) )
+    mOscMessage = OSCMessage()
+
+    headset = Emotiv(serial_number="SN201405", vendor_id=0x21a1, product_id=0x0001)
+    gevent.spawn(headset.setup)
+    gevent.sleep(0)
+
+    try:
+        while True:
+            packet = headset.dequeue()
+            for k,v in packet.sensors.iteritems():
+                if k in 'AF3 F7 F3 FC5 T7 P7 O1 O2 P8 T8 FC6 F4 F8 AF4':
+                    for kk,vv in v.iteritems():
+                        mOscMessage.clear("/potencial-accion/"+k+"/"+kk)
+                        mOscMessage.append(vv)
+                        mOscClient.send(mOscMessage)
+
+            gevent.sleep(0)
+    except KeyboardInterrupt:
+        headset.close()
+    finally:
+        headset.close()
